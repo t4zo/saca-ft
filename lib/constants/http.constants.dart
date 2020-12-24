a138bb73c27411b0af7b69c20395b513a1ce31c7
Tacio de Souza Campos <taciodesouzacampos@gmail.com>
2020-12-24 15:00:20 -0300

    updating server ip

--- lib/constants/http.constants.dart
+++ lib/constants/http.constants.dart
@@ -2,5 +2,5 @@ class HttpConstants {
   static const CODE_NOT_200 = 'Dio status code is not 200';
 
   // static const API_URL = 'http://10.0.2.2:5800/saca/v2';
-  static const API_URL = 'http://167.99.20.239:5500/saca/v2';
+  static const API_URL = 'http://159.89.244.218:5500/saca/v2';
 }
